fc44287f97a4981efb62342a049b4476cd8f8dcd
Code Austin 9 <630smileaa@gmail.com>
2018-07-05 20:57:43 -0600

    added images

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,6 +5,17 @@ class Home extends React.Component {
             <div>
                 <h1>Home Page</h1>
             </div>
+//             <div>
+//             <p>
+//                 Since 1999 our company has been committed to provide for the paintball industry Quality products for fields, professional players and beginners.
+// Over the years we have formed a team of professionals dedicated to the different lines of business of our company with the commitment of the best service and attention to our distributors and end users.
+ 
+// At Inzane Paintball we are guided by our values: Honesty, Responsibility, Improvement, and Social Commitment. In this way we have established ourselves as a serious and responsible company that generates prosperous businesses for the Paintball industry.
+ 
+// We are Inzane for Paintball, are you InZane yet?
+//            </p>  
+//            </div>   
+                
         )
     }
 }
--- src/products/index.js
+++ src/products/index.js
@@ -1,23 +1,44 @@
 export const products = [
     {
         id: 1,
-        image: "#",
+        image: "http://inzanepb.com/____impro/1/onewebmedia/Imagen_1.png?etag=W%2F%2219d54-5a8c7d48%22&sourceContentType=image%2Fpng&ignoreAspectRatio&resize=341%2B258",
         name: "First Rental",
         level: "Basic Level",
         price: 19.99
     },
     {
         id: 2,
-        image: "#",
+        image: "http://inzanepb.com/____impro/1/onewebmedia/Imagen2.png?etag=W%2F%221c708-598a5557%22&sourceContentType=image%2Fpng&ignoreAspectRatio&resize=341%2B258",
         name: "First Eko",
         level: "Entry Level",
         price: 19.99
     },
     {
         id: 3,
-        image: "#",
-        name: "First Rental",
+        image: "https://psycogotcha.com/wp-content/uploads/2014/12/800504.jpg",
+        name: "First Hard",
         level: "Entry Level",
+        price: 19.99
+    },
+    {
+        id: 3,
+        image: "https://psycogotcha.com/wp-content/uploads/2014/12/800505.jpg",
+        name: "Maddness",
+        level: "Intermediate/Training",
+        price: 19.99
+    },
+    {
+        id: 3,
+        image: "http://inzanepb.com/____impro/1/onewebmedia/Imagen5.png?etag=W%2F%2224b69-598a555a%22&sourceContentType=image%2Fpng&ignoreAspectRatio&resize=341%2B256",
+        name: "Maddness Zero",
+        level: "Performance",
+        price: 19.99
+    },
+    {
+        id: 3,
+        image: "https://psycogotcha.com/wp-content/uploads/2014/12/800507.jpg",
+        name: "Demented",
+        level: "Tournament",
         price: 19.99
     },
 ]
