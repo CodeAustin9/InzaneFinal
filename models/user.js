7d0548bf1f5d5c5c8012f26a556c5fbf45627e9f
Code Austin 9 <630smileaa@gmail.com>
2018-07-06 11:24:46 -0600

    added partial authentication

---
+++ models/user.js
@@ -0,0 +1,65 @@
+// Requiring bcrypt for password hashing. Using the bcrypt-nodejs version as the regular bcrypt module
+// sometimes causes errors on Windows machines
+var bcrypt = require("bcrypt-nodejs");
+// Creating our User model
+// module.exports = function (sequelize, DataTypes) {
+//     var User = sequelize.define("User", {
+//         // The email cannot be null, and must be a proper email before creation
+//         email: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//             unique: true,
+//             validate: {
+//                 isEmail: true
+//             }
+//         },
+//         // The password cannot be null
+//         password: {
+//             type: DataTypes.STRING,
+//             allowNull: false
+//         }
+//     });
+//     // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+
+//     // Hooks are automatic methods that run during various phases of the User Model lifecycle
+//     // In this case, before a User is created, we will automatically hash their password
+
+
+
+
+// Require mongoose
+var mongoose = require("mongoose");
+
+// Create a schema class using mongoose's schema method
+var Schema = mongoose.Schema;
+
+// Create the headlineSchema with our schema class
+var usernameSchema = new Schema({
+  // headline, a string, must be entered
+  email: {
+    type: String,
+    required: true,
+    unique: { index: { unique: true } }
+  },
+  // summary, a string, must be entered
+  password: {
+    type: String,
+    required: true
+  },
+ 
+  
+});
+
+// Create the Headline model using the headlineSchema
+var User = mongoose.model("User", usernameSchema);
+
+// User.prototype.validPassword = function (password) {
+//     return bcrypt.compareSync(password, this.password);
+// };
+
+// User.hook("beforeCreate", function (user) {
+//     user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+// });
+
+// Export the Headline model
+module.exports = User;
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,15 +1,37 @@
 import React from "react";
 class Login extends React.Component {
+    state={
+        username: "",
+        password: "",
+    }
+    handleChange=(event)=>{ 
+        const {name, value} = event.target;
+        this.setState({
+            [name]:value
+        })
+       }
+    handleSubmit=(event)=>{
+        event.preventDefault();
+        console.log(this.state.username);
+        console.log(this.state.password);
+    }
+
     render() {
         return (
             <div>
                 <h1>Login Page</h1>
-                
+                <form>
+                    <label>Username</label>
+                    <input type="text" name="username" onChange={this.handleChange} value={this.state.username}/>
+                    <br/>
+                    <label>Password</label>
+                    <input type="text" name="password" onChange={this.handleChange} value={this.state.password}/>
+                    <br/>
+                    <button type="submit" onClick={this.handleSubmit}>Submit</button>
          
-          <h2>Username</h2>
-          <h2>Password</h2>
+
     
-            
+            </form>
         </div>
 
            
