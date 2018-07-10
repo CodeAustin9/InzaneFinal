3db34e48d8c0f6aef54de0c7db891bdcb816c931
Code Austin 9 <630smileaa@gmail.com>
2018-07-10 11:45:41 -0600

    adding formatting to login page

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./login.css";
 
 class Login extends React.Component {
     state={
@@ -19,23 +20,30 @@ class Login extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="body">
                 <h3><center>Login</center></h3>
                 <form>
-                    <div>
+                    <div className="labels">
                     <label><h5><center><strong>Username:</strong></center> </h5> </label>
+                    </div>
+                   <div className="input">
                     <input type="text" name="username" onChange={this.handleChange} value={this.state.username}/>
                    </div>
-                    <br/>
+                   
+                   
                     <div>
                     <label><h5><center><strong>Password:  </strong></center></h5></label>
+    
                     <input type="text" name="password" onChange={this.handleChange} value={this.state.password}/>
                    </div>
+                  
+                   
 
                     <br/>
                     <center><button type="submit" onClick={this.handleSubmit}>Submit</button></center>
             </form>
         </div>
+        
 
            
         )
